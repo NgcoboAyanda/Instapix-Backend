5dc221cf352cdbc0391f3d8775008eb17d9dbb60
NgcoboAyanda <ayanda13@protonmail.com>
2023-01-02 18:31:09 +0200

    added sqlite3 db

--- project/settings.py
+++ project/settings.py
@@ -127,7 +127,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': "django.db.backends.sqlite3",
+        'NAME': BASE_DIR / 'db.sqlite3'
+    }
+    #'default': dj_database_url.config()
     #{
     #    'ENGINE': 'django.db.backends.postgresql',
     #    'NAME': "instapixbackend",
